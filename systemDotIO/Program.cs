da030bd75aea12e2ee5d8b034071d21a4fadd83c
DennisVN <78357110+DennisVN@users.noreply.github.com>
2022-01-03 15:10:36 +0100

    Array LIFO

--- systemDotIO/Program.cs
+++ systemDotIO/Program.cs
@@ -7,6 +7,7 @@ namespace systemDotIO
     {
         static void Main(string[] args)
         {
+            #region Queue FIFO
             // Queue (First in First out !)
 
             /* Use Queue if you need to access the information in the same order that it is stored in the collection. 
@@ -20,15 +21,44 @@ namespace systemDotIO
             l.Enqueue(123);
             l.Enqueue(true);
             object o = l.Peek();
-
+            Console.WriteLine("List of Queue Values : ");
+            PrintValues(l);
 
             Console.WriteLine(l.Count);
-            Console.WriteLine((string) o);
-            Console.WriteLine(l.Count);
+            Console.WriteLine((string)o);
             o = l.Dequeue();
             Console.WriteLine(l.Count);
             Console.WriteLine((string)o);
-            Console.WriteLine(l.Count);
+            Console.WriteLine("List of Queue Values : ");
+            PrintValues(l);
+            #endregion
+
+            #region Stack LIFO
+            // Stack ( Last in First Out ) 
+            // Peek() Return the object at the top of the stack without removing it 
+            // Pop() Removes and returns the object at the Top of the Stack
+            // Push(object) Insert an object at the top of the Stack
+            //System.Collections.Stack myStack = new Stack();
+            //myStack.Push("def");
+            //myStack.Push(456);
+            //myStack.Push(true);
+
+            //Console.WriteLine(myStack.Count);   // 3
+            //Console.WriteLine(myStack.Peek()); // True
+            //Console.WriteLine("List of Stack values : ");
+            //PrintValues(myStack);
+
+            #endregion
+
+            Console.ReadLine();
+        }
+        public static void PrintValues (IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+            {
+                Console.Write(" {0}", obj);
+                Console.WriteLine();
+            }
         }
     }
 }
